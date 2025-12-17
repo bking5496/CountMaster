5288b4742f5a34f81ebf16f8b50237be22ffcde8
Brandon <bking5496@gmail.com>
2025-12-17 15:55:23 +0200

    Add Supabase credentials (anon key is public by design)

---
+++ js/env.js
@@ -0,0 +1,5 @@
+// Environment configuration
+window.env = {
+    SUPABASE_URL: 'https://exltxjvzsefmaxlgxyio.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4bHR4anZ6c2VmbWF4bGd4eWlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0NTQ2MjcsImV4cCI6MjA4MDAzMDYyN30.kc5RFVrN5orQ7AqPd51ot-sK1xbWsy_58ToowCvYhZw'
+};
